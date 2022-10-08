11e6b13bc9a53af5e97813c324152f62ea8ff2fc
Shell <72198971+Shell1010@users.noreply.github.com>
2022-10-08 01:54:53 +0100

    Initial commit - Started bot class, began static login, integrated really bad old gateway which I will be fixing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from selfcord.bot import Bot
 import asyncio
+import json
 
 bot = Bot()
 
+with open("./config.json", "r") as f:
+    config = json.load(f)
+
+token = config.get("token")
+
 async def main():
-    await bot.run("")
+    await bot.run(token)
 
 
 
