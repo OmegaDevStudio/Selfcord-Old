42684b28e160c1feb7f2ecd47ede3a1b97839642
Shell <72198971+Shell1010@users.noreply.github.com>
2022-10-24 16:13:44 +0100

    Updated tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -10,7 +10,7 @@ class Tests:
 
     def test(self):
         try:
-            bot.add_cmd(coro=test, description="Sends hello!", aliases=["hello"])
+            bot.add_cmd(coro=self.test, description="Sends hello!", aliases=["hello"])
         except Exception as e:
             raise RuntimeError(f"Failed to add command. Error: {e}")
 
