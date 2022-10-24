5476d11580b8eaf4ed1bcc29b4378d370ede3ed9
Shell <72198971+Shell1010@users.noreply.github.com>
2022-10-24 16:21:50 +0100

    Updated tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,16 +4,16 @@ import selfcord
 bot = selfcord.Bot(prefixes=["!"])
 
 @pytest.mark.asyncio
+class Tests:
+    async def test(self, *args):
+        print("HELLO WORLD", args)
 
-async def test(*args):
-    print("HELLO WORLD", args)
+    def add_cmd_test(self):
+        try:
+            bot.add_cmd(coro=self.test, description="Sends hello!", aliases=["hello"])
+        except Exception as e:
+            raise RuntimeError(f"Failed to add command. Error: {e}")
 
-def add_cmd_test(self):
-    try:
-        bot.add_cmd(coro=self.test, description="Sends hello!", aliases=["hello"])
-    except Exception as e:
-        raise RuntimeError(f"Failed to add command. Error: {e}")
-
-    cmds = len(bot.commands)
-    print(cmds)
-    assert cmds > 0
+        cmds = len(bot.commands)
+        print(cmds)
+        assert cmds > 0
